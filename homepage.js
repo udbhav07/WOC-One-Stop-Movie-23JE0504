d3bfa101c6633187fda9667dd8ccc3b1a933ce21
udbhav07 <85992630+udbhav07@users.noreply.github.com>
2024-01-07 17:37:20 +0530

    added tags

--- homepage.js
+++ homepage.js
@@ -41,6 +41,131 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
+const genres =
+   [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ];
+  const tags = document.getElementById('tags');
+  getGenres();
+  
+  var selectedTags =[];
+
+function getGenres() {
+  tags.innerHTML="";
+  genres.forEach(genre=>{
+    const tag = document.createElement('div');
+    tag.classList="tag"
+    tag.id=genre.id;
+    tag.innerText=genre.name;
+    tag.addEventListener("click",()=>{
+      if(selectedTags.length==0){selectedTags.push(genre.id)}
+      else{
+        if(selectedTags.includes(genre.id)){
+          selectedTags.forEach((id,index)=>{
+            if( id == genre.id){
+              selectedTags.splice(index,1);
+            }
+          })
+
+        }else{
+          selectedTags.push(genre.id);
+        }
+      }
+      console.log(selectedTags);
+      getMovies(API_URL+"&with_genres="+selectedTags.join(','))
+
+    })
+    tags.append(tag);
+    
+  })
+
+}
+
+
+function highlight(){
+  if (selectedTags.length!=0){
+    selectedTags.forEach(id=>{
+      const highlightedTag = document.getElementById(id);
+      highlightedTag.classList.add("highlight")
+    })
+  }
+}
+
 
 const main = document.getElementById("main")
 const search = document.getElementById("search")
